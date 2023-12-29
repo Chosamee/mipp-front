be8d3115f60807bd0681e2acf4c61264fbf1488c
Chosamee <cytor@naver.com>
2023-12-30 03:21:35 +0900

    fix: Fix responsible design

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -40,22 +40,24 @@ const App = () => {
 
         {/* Overlay Content */}
         <div className="bg-index-gradient absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center">
-          <div className="grid grid-cols-2 max-w-7xl text-center text-white p-4">
-            <FadeInComp
-              data={
-                <div className="text-2xl md:text-9xl font-bold mb-2">
-                  <div className="text-9xl my-8">표절 검사</div>
-                  <div className="text-7xl my-8">검은 사막</div>
-                  <div className="text-8xl my-8">나도 (안) 한다</div>
-                </div>
-              }
-              animate={"fade-in-up-delay-1"}
-            />
+          <div className="grid md:grid-cols-2 max-w-7xl text-center text-white py-4 px-10">
+            <div className="hidden md:block">
+              <FadeInComp
+                data={
+                  <div className="text-2xl md:text-9xl font-bold mb-2">
+                    <div className="text-9xl my-8">표절 검사</div>
+                    <div className="text-7xl my-8">검은 사막</div>
+                    <div className="text-8xl my-8">나도 (안) 한다</div>
+                  </div>
+                }
+                animate={"fade-in-up-delay-1"}
+              />
+            </div>
             <div className="flex flex-col justify-center">
               <FadeInComp
                 data={
                   <p className="mb-4">
-                    표절 검사를 하세요 여러분들의 소리를 들려주세요 으악하하하
+                    표절 검사를 하세요 여러분들의 소리를 들려주세요
                     <br />
                     하라면 하세요 텍스트를 넣을게 없어요 어떡해야할까요
                   </p>
@@ -83,11 +85,11 @@ const App = () => {
       </div>
 
       {/* Process Section */}
-      <div className="container mx-auto rounded-lg mb-20 mt-5 p-4 text-center">
+      <div className="container mx-auto rounded-lg mb-20 mt-5 py-4 px-4 text-center">
         <h2 className="text-3xl font-bold mb-1 mt-3 ">사용 PROCESS</h2>
         {/* Assuming indexContent1 is imported correctly at the top */}
         <FadeInComp
-          data={<img src={indexContent1} alt="Description" className="mx-auto rounded w-3/4" />}
+          data={<img src={indexContent1} alt="Description" className="mx-auto rounded" />}
           animate={"fade-in-right-delay-1"}
         />
       </div>
@@ -113,18 +115,20 @@ const App = () => {
               <FadeInComp
                 data={
                   <div>
-                    <p className="mb-5 text-3xl font-bold">01. AI 음악 분석 검사</p>
-                    <p className="mb-10 text-lg">
+                    <p className="mb-5 md:text-3xl text-xl font-bold">01. AI 음악 분석 검사</p>
+                    <p className="mb-10 md:text-lg text-sm">
                       MIPP만의 음원 분석 기술을 사용한 AI 음악 표절 검사를 무료로 해보세요 앨범 발매
                       전 표절로 인한 법적인 문제를 사전에 예방할 수 있습니다.
                     </p>
-                    <p className="mb-5 text-3xl font-bold">02. 실시간 표절노래 모니터링</p>
-                    <p className="mb-10 text-lg">
+                    <p className="mb-5 md:text-3xl text-xl font-bold">
+                      02. 실시간 표절노래 모니터링
+                    </p>
+                    <p className="mb-10 md:text-lg text-sm">
                       전 세계에 새로 발매 되는 수억개의 노래 중에 내 노래를 표절한 노래가 있는지
                       찾아드립니다. MIPP 실시간 표절 모니터링 서비스로 귀하의 음악 저작권을 지키세요
                     </p>
-                    <p className="mb-5 text-3xl font-bold">03. 음악 IP 수익 극대화</p>
-                    <p className="mb-10 text-lg">
+                    <p className="mb-5 md:text-3xl text-xl font-bold">03. 음악 IP 수익 극대화</p>
+                    <p className="mb-10 md:text-lg text-sm">
                       MIPP을 통해 무단 표절 노래로 부터 저작권을 보호 하고 음악 IP의 수익을 극대화
                       시켜보세요
                     </p>
