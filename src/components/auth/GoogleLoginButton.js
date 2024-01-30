65342f9476113e1d58b13c90a1ab54b66de93f5b
Chosamee <cytor@naver.com>
2024-01-31 02:54:27 +0900

    fix: Fix axios.get

--- src/components/auth/GoogleLoginButton.js
+++ src/components/auth/GoogleLoginButton.js
@@ -7,10 +7,8 @@ import { useTranslation } from "react-i18next";
 import { getLangUrl } from "locales/utils";
 
 const GoogleLoginButton = () => {
-  const { i18n } = useTranslation();
-
   const initiateLogin = async () => {
-    const redirect = await handleSessionState(i18n.language);
+    const redirect = await handleSessionState(navigator.language.split("-")[0]);
     window.location.href = redirect;
   };
   const { authState, updateAuthState } = useAuth();
