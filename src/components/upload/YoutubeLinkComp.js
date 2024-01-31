43a90a4c5a79fa33de5813c637259214f96a32f9
Chosamee <cytor@naver.com>
2024-01-31 10:18:56 +0900

    fix: Fix login and home design

--- src/components/upload/YoutubeLinkComp.js
+++ src/components/upload/YoutubeLinkComp.js
@@ -35,17 +35,21 @@ const YoutubeLinkComp = ({ inst, bpm }) => {
 
   return (
     <React.Fragment>
-      <div className="mb-5 w-[420px] text-start">{t("home.linkGuide")}</div>
       <input
         type="text"
         value={inputValue}
         onChange={handleInputChange}
         placeholder={t("home.linkInputGuide")}
-        className="px-4 py-2 border rounded-full mb-4 w-full border-black"
+        className="px-[33px] py-3 border rounded-full mb-[14px] w-full border-black"
       />
+      <div className="flex mb-12 w-[326px] desktop:w-[420px] text-start">
+        <div className="text-[#D95E59] text-[14px] desktop:text-[16px] mr-[3px]">*</div>
+        <div className="text-[#A0A0A0] text-[13px] desktop:text-[14px]">{t("home.linkGuide")}</div>
+      </div>
+
       <button
         onClick={handleSubmit}
-        className="w-[420px] py-[20px] bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-offset-2">
+        className="w-[326px] desktop:w-[420px] py-[20px] bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-offset-2">
         {t("home.submit")}
       </button>
     </React.Fragment>
