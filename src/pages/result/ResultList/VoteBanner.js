87c962325313ce60ad07c9dc52c0c195e11dc838
Chosamee <cytor@naver.com>
2024-02-28 02:23:34 +0900

    fix: 설문 언어

--- src/pages/result/ResultList/VoteBanner.js
+++ src/pages/result/ResultList/VoteBanner.js
@@ -6,7 +6,7 @@ const VoteBanner = ({ vote }) => {
   const { i18n } = useTranslation();
   const handleVoteClicked = () => {
     const voteUrl =
-      i18n === "en"
+      i18n.language === "en"
         ? "https://docs.google.com/forms/d/1-gonui98i9BmJmGzfxrTSJJNBNNvBo7FMeuG-Se_e6E/edit"
         : "https://docs.google.com/forms/d/1Iw5ofDxmmATG5xtOUs_CnBF-SLpjBJvszQk16qOZwsM/edit";
     window.open(voteUrl, "_blank");
@@ -17,11 +17,23 @@ const VoteBanner = ({ vote }) => {
       <div className="flex flex-col desktop:flex-row w-[326px] desktop:w-fit justify-center mx-auto ">
         <div className="flex flex-col" style={{ fontFeatureSettings: "'ss10' on" }}>
           <p className="text-[#E2E2E3] text-base leading-[27px] font-semibold mb-[10px] text-nowrap">
-            검사 결과는 어떠셨나요? <br className="desktop:hidden" />
-            사용 후기에 대한 의견을 알려주세요!
-            <br />
-            설문조사 내용를 통해 <br className="desktop:hidden" />
-            서비스를 지속적으로 개선합니다.
+            {i18n.language === "en" ? (
+              <>
+                How was your experience with the test? <br className="desktop:hidden" />
+                We'd love to hear your feedback!
+                <br />
+                Your responses to this survey <br className="desktop:hidden" />
+                will help us continually improve our service.
+              </>
+            ) : (
+              <>
+                검사 결과는 어떠셨나요? <br className="desktop:hidden" />
+                사용 후기에 대한 의견을 알려주세요!
+                <br />
+                설문조사 내용를 통해 <br className="desktop:hidden" />
+                서비스를 지속적으로 개선합니다.
+              </>
+            )}
           </p>
           <button
             className="flex gap-[10px] items-center w-fit"
@@ -29,7 +41,7 @@ const VoteBanner = ({ vote }) => {
               handleVoteClicked();
             }}>
             <p className="text-[#8296FF] text-base font-semibold leading-[27px]">
-              설문조사 하러가기
+              {i18n.language === "en" ? "Go to the Survey" : "설문조사 하러가기"}
             </p>
             <RightArrowIcon />
           </button>
