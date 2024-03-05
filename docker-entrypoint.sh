f80d42df095072736a9c2df4e4fdfeee06120f7c
Chosamee <cytor@naver.com>
2024-03-05 15:53:26 +0900

    fix: Fix docker-enrtypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # envsubst를 사용하여 nginx.conf 생성
-envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${PRERENDER_TOKEN}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Nginx 시작
 exec nginx -g 'daemon off;'
