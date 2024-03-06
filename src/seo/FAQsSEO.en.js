cbcc629656369c400c180c1807bdcaa7c50ddece
Chosamee <cytor@naver.com>
2024-03-06 17:31:55 +0900

    fix: Fix nginx.conf

--- src/seo/FAQsSEO.en.js
+++ src/seo/FAQsSEO.en.js
@@ -5,17 +5,17 @@ const FAQsSEOEN = () => {
   return (
     <Helmet>
       <title>MIPP - FAQs</title>
-      <meta name="description" content="Find answers to frequently asked questions about MIPP's music plagiarism detection service. Get clarity on your queries to enhance your experience." />
+      <meta name="description" content="Find answers to frequently asked questions about MIPP's music plagiarism detection service. Get clarity on your questions to enhance your experience." />
 
       <meta property="og:title" content="MIPP - FAQs" />
-      <meta property="og:description" content="Find answers to frequently asked questions about MIPP's music plagiarism detection service. Get clarity on your queries to enhance your experience." />
+      <meta property="og:description" content="Find answers to frequently asked questions about MIPP's music plagiarism detection service. Get clarity on your questions to enhance your experience." />
       <meta property="og:type" content="website" />
       <meta property="og:image" content="https://storage.googleapis.com/domainbucket/imageog.png" />
       <meta property="og:url" content="https://aimipp.com/en/faqs" />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content="MIPP - FAQs" />
-      <meta name="twitter:description" content="Find answers to frequently asked questions about MIPP's music plagiarism detection service. Get clarity on your queries to enhance your experience." />
+      <meta name="twitter:description" content="Find answers to frequently asked questions about MIPP's music plagiarism detection service. Get clarity on your questions to enhance your experience." />
       <meta name="twitter:image" content="https://storage.googleapis.com/domainbucket/imageog.png" />
     </Helmet>
   );
