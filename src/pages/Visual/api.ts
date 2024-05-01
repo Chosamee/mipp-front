c1e3cc3c5c8d0a4d36248cd249e23c28bdaa7e2b
Chosamee <cytor@naver.com>
2024-05-01 13:08:59 +0900

    add: Add view copy link

--- src/pages/Visual/api.ts
+++ src/pages/Visual/api.ts
@@ -3,8 +3,14 @@ import { IVisualData } from "./types";
 
 const API_BASE_URL = process.env.REACT_APP_MIPP_API_URL;
 
-export const fetchVisualData = async (result_id: number): Promise<IVisualData> => {
-  const response = await axios.put(`${API_BASE_URL}/visualize/${result_id}`, null, {
+export const fetchVisualData = async (
+  result_id: number,
+  token: string | null
+): Promise<IVisualData> => {
+  const url = token
+    ? `${API_BASE_URL}/visualize/${result_id}?token=${token}`
+    : `${API_BASE_URL}/visualize/${result_id}`;
+  const response = await axios.put(url, null, {
     withCredentials: true,
   });
   return response.data;
