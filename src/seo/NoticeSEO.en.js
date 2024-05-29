0eaf657b2617c0903006beb391ede282a88b46cd
seonghyeon <skskskrhfem@naver.com>
2024-05-29 20:23:20 +0900

    transfer to mippia

--- src/seo/NoticeSEO.en.js
+++ src/seo/NoticeSEO.en.js
@@ -4,19 +4,21 @@ import { Helmet } from "react-helmet";
 const NoticeSEOEN = () => {
   return (
     <Helmet>
-      <title>MIPP - Notice</title>
-      <meta name="description" content="MIPP" />
+      <title>MIPPIA - Notice</title>
+      <meta name="description" content="MIPPIA" />
 
-      <meta property="og:title" content="MIPP - Notice" />
-      <meta property="og:description" content="Important notices about MIPP services." />
+      <meta property="og:title" content="MIPPIA - Notice" />
+      <meta property="og:description" content="Important notices about MIPPIA services." />
       <meta property="og:type" content="website" />
-      <meta property="og:image" content="https://storage.googleapis.com/domainbucket/imageog.png" />
-      <meta property="og:url" content="https://aimipp.com/en/notice" />
+      <meta property="og:image" content="https://storage.googleapis.com/mippia-domain-bucket/imageog.png" />
+      <meta property="og:url" content="https://mippia.com/en/notice" />
 
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="MIPP - Notice" />
-      <meta name="twitter:description" content="Important notices about MIPP services." />
-      <meta name="twitter:image" content="https://storage.googleapis.com/domainbucket/imageog.png" />
+      <meta name="twitter:title" content="MIPPIA - Notice" />
+      <meta name="twitter:description" content="Important notices about MIPPIA services." />
+      <meta name="twitter:image" content="https://storage.googleapis.com/mippia-domain-bucket/imageog.png" />
+
+      <link rel="canonical" href="https://mippia.com/en/notice" />
     </Helmet>
   );
 };
